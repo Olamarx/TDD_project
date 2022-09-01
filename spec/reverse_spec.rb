198e9cd3df75e62b5e4c2b529c3868d2e797888a
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-09-02 00:16:49 +0100

    imported the file i created to the test file and changed the form of equal to that i initially wrote

--- spec/reverse_spec.rb
+++ spec/reverse_spec.rb
@@ -1,3 +1,5 @@
+require './solver'
+
 describe Solver do
   context 'When testing reverse method' do
     before :each do
@@ -5,19 +7,19 @@ describe Solver do
     end
     it "Should return 'drawkcab daer lliw sihT' if passes 'This will read backward' as argument" do
       result = @solver.reverse('This will read backward')
-      expect(result).to eq 'drawkcab daer lliw sihT'
+      expect(result) == 'drawkcab daer lliw sihT'
     end
     it "Should return '0,9,8,7,6,5,4,3,2,1' if pass '1,2,3,4,5,6,7,8,9,0' as argument" do
       result = @solver.reverse('1,2,3,4,5,6,7,8,9,0')
-      expect(result).to eq '0,9,8,7,6,5,4,3,2,1'
+      expect(result) == '0,9,8,7,6,5,4,3,2,1'
     end
     it "Should return 'ZYXWVUTSRQPONMLKJIHGFEDCBA' if pass 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' as argument" do
       result = @solver.reverse('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-      expect(result).to eq 'ZYXWVUTSRQPONMLKJIHGFEDCBA'
+      expect(result) == 'ZYXWVUTSRQPONMLKJIHGFEDCBA'
     end
     it "Should return 'r e l i o p s' if passes 's p o i l e r' as argument" do
       result = @solver.reverse('s p o i l e r')
-      expect(result).to eq 'r e l i o p s'
+      expect(result) == 'r e l i o p s'
     end
   end
 end
